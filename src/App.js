ff0469587dece14116cae00881e32fc9eb45cd2e
Mathias.Mouillard <Mouillardpro@gmail.com>
2024-02-16 16:49:26 +0100

    Error page first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 //import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
+import "./styles/App.css";
+import "./styles/variables.css";
 import Home from "./pages/Home/Home";
-//import Rental from './pages/Rental'
-import About from './pages/About/About'
-//import Error from './pages/Error'
+import Rental from "./pages/Rental/Rental";
+import About from "./pages/About/About";
+import Error from "./pages/Error/Error";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
@@ -17,6 +18,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/About" element={<About />} />
+          <Route path="/Rental" element={<Rental />} />
+          <Route path="*" element={<Error />} />
         </Routes>
         <Footer />
       </div>
